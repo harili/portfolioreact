f944e02e377adb95158b4c4b14be7ec7d3426a56
harili <ilieschafyfy@gmail.com>
2020-08-16 23:02:34 +0200

    Updates

---
+++ precache-manifest.c248632e1690a5f0472ca7c935d5475d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbbb23e8fd1d6a403a8e73657205b8b7",
+    "url": "/portfolioreact/index.html"
+  },
+  {
+    "revision": "f10478c0ca429d670eda",
+    "url": "/portfolioreact/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "117a3268cdc6467e493f",
+    "url": "/portfolioreact/static/js/2.cc4c0fc2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolioreact/static/js/2.cc4c0fc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f10478c0ca429d670eda",
+    "url": "/portfolioreact/static/js/main.2e133dfc.chunk.js"
+  },
+  {
+    "revision": "af4a19abcc2cceb3966a",
+    "url": "/portfolioreact/static/js/runtime-main.ab59ef44.js"
+  }
+]);
